e4eb69bb8da20d394e9e8881aa7cc78b760849ee
alexandr <alexunder2014@yandex.ru>
2022-09-10 13:39:26 +0900

    добавим файл игнор в задачу 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int Prompt(string messege)
+{
+    System.Console.WriteLine(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int Value1 = Prompt("Введите a");
+int Value2 = Prompt("Введите b");
+int Value3 = Prompt("Введите c");
+if (Value1 > Value2 && Value1> Value3)
+{
+    System.Console.WriteLine("a - максимальное");
+}
+if (Value2 > Value1 && Value2> Value3)
+{
+    System.Console.WriteLine("b - максимальное");
+}
+if (Value3 > Value1 && Value3  > Value2)
+{
+    System.Console.WriteLine("c - максимальное");
+}
