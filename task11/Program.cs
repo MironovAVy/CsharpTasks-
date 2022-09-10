edcff7cb5bfad6c68cd1d0199eaba779b30b4b78
alexandr <alexunder2014@yandex.ru>
2022-09-10 14:00:36 +0900

    задача 11 решение

---
+++ task11/Program.cs
@@ -0,0 +1,19 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Cube(Prompt("Введите целое число: "));
+void Cube(int value)
+{
+    for (int i = 1; i <= value; i++)
+    {
+        System.Console.Write(Math.Pow(i, 3));
+        if (i < value)
+        {
+            System.Console.Write(",");
+        }
+       
+    }
+}
