69b7a3e444853c1a74b85d7463266511633c94a9
alexandr <alexunder2014@yandex.ru>
2022-09-10 13:48:29 +0900

    задача 6 решение

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿ //Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int number = new Random().Next(99,1000);
+void second()
+{
+int p = number;
+double a = p%10;
+double b = p%100;
+System.Console.WriteLine($"Ваше число ={p}");
+System.Console.WriteLine($"Ваше число без второй цифры равно = { p/10 - (b-a)/10 + a}");
+}
+second();
+
