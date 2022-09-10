3e91d1e5000a4715dc6637aeada3ae8ec334e47a
alexandr <alexunder2014@yandex.ru>
2022-09-10 14:34:20 +0900

    задача со звездочкой

---
+++ zvezdoshka/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая генерирует несколько случайных чисел, и в цикле проверяет, кратны ли эти числа предварительно введенному числу, при кратности - цикл прерывается.
+int Prompt(string messege)
+{
+    System.Console.WriteLine(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int value_a = Prompt("Введите число");
+Random rnd = new Random();
+int p = rnd.Next();
+int b =  rnd.Next();
+if (p % value_a  == 0 && b % value_a  == 0 )
+{
+System.Console.WriteLine($"Число {p} и {b} делиться на {value_a}");
+}
+else
+{
+    System.Console.WriteLine($"Числа {p} и {b} не кратные {value_a}");
+}
+
