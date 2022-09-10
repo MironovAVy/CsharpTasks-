e4fc94063ff52ced616cad337bd69137307d729c
alexandr <alexunder2014@yandex.ru>
2022-09-10 13:46:54 +0900

    задача 5 решение

---
+++ task5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string messege)
+{
+    System.Console.WriteLine(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int Value = Prompt("Введите трех значное число");
+void second()
+{
+int p = Value;
+int a = p%10;
+int b = p%100;
+if (p > 99 && p < 1000)
+{
+    System.Console.WriteLine($"Ваше число ={p}");
+    System.Console.WriteLine($"Второе число равно = {(b-a)/10}");
+}
+else
+{
+    System.Console.WriteLine($"Ваше число - не трехзначное");
+} 
+}
+second();
