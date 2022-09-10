ba0ba28f4f15a62235ca96d70de8820565b95595
alexandr <alexunder2014@yandex.ru>
2022-09-10 13:53:19 +0900

    задача 8 решение

---
+++ task8/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int Prompt(string messege)
+{
+    System.Console.WriteLine(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int Value = Prompt("Введите  число");
+void second()
+{
+int p = Value;
+if (p > 5 && p < 7)
+{
+    System.Console.WriteLine($"Ваше число {p} - выходной");
+}
+if (p > 0 && p <= 5)
+{
+    System.Console.WriteLine($"Ваше число {p} - будний день");
+} 
+else
+{
+    System.Console.WriteLine($"{p} -такого дня недели не существует ");
+}
+}
+second();
